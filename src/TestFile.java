220429b0d0d4c3f5cd185fab536299d577d9ae05
Rares Gudea <rares.gudea@gmail.com>
2019-04-28 07:25:58 +0300

    x

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,11 +1,10 @@
 public class TestFile {
 
     //test comment
-<<<<<<< HEAD
+
 
 // coment 2 pc
-=======
-    
+
     //this is an edit in browser 2
->>>>>>> d9e43ef9cdc3389ae9002c0efbc11d2a1d0f0a4e
+
 }
