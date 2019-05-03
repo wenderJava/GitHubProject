41f831319d2048ec79c95a792d7a983a8be99337
Rares Gudea <rares.gudea@gmail.com>
2019-05-03 15:21:07 +0300

    101010

---
+++ src/ParentClass.java
@@ -0,0 +1,6 @@
+public class ParentClass {
+   public static void main(String[] args){
+        System.out.println("'having a aline with more than 80 caracters to " +
+                "check wrap text");
+   }
+}
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,10 +1,42 @@
 public class TestFile {
 
-    //test comment
+    static int[] v = {0,1,0,0,1,1,1,0};
+
+    public static void main(String[] args){
+        int i,j;
+        int transfer;
+        boolean inversare = true;
+
+        printArray(v);
+        for(i=0;i<v.length-1;i++){
+
+            if(v[i]== v[i+1]){
+                for(j=i+1;j<v.length;j++){
+                    if(v[j]!=v[i]){
+                        transfer = v[j];
+                        v[j] =v[j-1];
+                        v[j-1]= transfer;
+                        i=-1;
+                        System.out.println();
+                        printArray(v);
+                        break;
+                    }
+
+                }
+            }
+
+        }
 
 
-// coment 2 pc
+    }
 
-    //this is an edit in browser 2
+
+    public static void printArray(int[] v){
+
+        for(int i =0; i<v.length;i++){
+            System.out.print(v[i]+" ");
+        }
+    }
 
 }
+
